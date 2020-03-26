8714f3687cc361a032823d3727ad91658f4fa643
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2020-03-25 19:58:58 -0500

    add comments

--- src/logica/Estudiante.java
+++ src/logica/Estudiante.java
@@ -1,3 +1,6 @@
+//Jorge Lievano
+//Leonardo Franco
+
 package logica;
 
 import java.util.Random;
@@ -5,10 +8,14 @@ import java.util.concurrent.Semaphore;
 
 public class Estudiante extends Thread{
 
+	// se declara la la relacion de cada estudiante con los semaforos 
 	private Semaphore sMonitor,sSalaEspera;
+	// se declara un generador de numeros aleatorios
 	private Random aleat;
+	// identificador del estudiante
 	private int id;
 	
+	// constructor con todos los argumentos para uns estudiante
 	public Estudiante(int id,Semaphore sMonitor,Semaphore sSalaEspera) {
 		super();
 		this.sMonitor=sMonitor;
@@ -17,26 +24,34 @@ public class Estudiante extends Thread{
 		this.id=id;
 	}
 	
+	
+	// metodo principal del hilo de cada estudiante
 	public void run() {
 		System.out.println("estudiante "+id+" Se creó");
+		//los estudiantes recien creados esperan 1s para tomar acción
 		try {
 			sleep(1000);
 		} catch (InterruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		// repetitivo infinito
 		while(true) {
 			try {
-				//System.out.println("estudiante "+id+" necesita monitoria");
+				// el estudiante revisa la sala de espera y pide un cupo
 				sSalaEspera.acquire();
 				System.out.println("estudiante "+id+" Esta en espera");
+				// si hay cupos en la sala de espera el estudiante asegura su turno con el monitor
 				sMonitor.acquire();
+				// una vez obtiene turno con el monitor se libera un cupo de la sala de espera 
 				sSalaEspera.release();
+				// el estudiante y el monitor se ocupan durante un tiempo
 				System.out.println("estudiante "+id+" Esta con el monitor");
 				sleep(10000);
+				// una vez el estudiante resuelve sus dudas deja libre al monitor
 				System.out.println("estudiante "+id+" Resolvió las dudas");
 				sMonitor.release();
-				//System.out.println("estudiante "+id+" va a Sala de computo");
+				//el monitor se va y tendra dudas en un tiempo
 				sleep(Math.abs(aleat.nextInt()* 1000) );
 			}catch(InterruptedException e){
 				
--- src/logica/Institucion.java
+++ src/logica/Institucion.java
@@ -1,3 +1,6 @@
+//Jorge Lievano
+//Leonardo Franco
+
 package logica;
 
 import java.util.ArrayList;
@@ -8,29 +11,25 @@ public class Institucion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		/*if (args.length != 1) {
-			System.out.println("Sintaxis: MonitorDormilon semilla_aleatoria");
-			System.exit(1);
-		}*/
-		
-		//long Semilla=Long.parseLong(args[0]);
-		
+		// se declaran lo semaforos que coordinan las interacciones
 		Semaphore monitor, salaEspera;
 		
+		// se inicializa una lista para un numero n de estudiantes
 		ArrayList<Estudiante>  estudiantes=new ArrayList<Estudiante>();
 		
-		//Estudiante est1,est2,est3,est4,est5,est6;
-		
-		
+		// se instancia el semaforo del monitor como binario para representar su estado ocupado/libre
 		monitor= new Semaphore(1,true);
+		// se instancia el semaforo de la sala de espera con n permisos segun el numero de sillas
 		salaEspera= new Semaphore(3,true);
 		
+		//se instancian y agregan a la lista los estudiantes
 		for (int i=1; i<7;i++) {
 			Estudiante est=new Estudiante(i, monitor, salaEspera);
 			estudiantes.add(est);
 		}
 		
 		
+		//se inician los hilos de cada estudiante
 		for(Estudiante e: estudiantes) {
 			e.start();
 		}
